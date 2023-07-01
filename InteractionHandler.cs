db6df32385ecc7b429201431217aa1cb6570de3b
ahmetkeremburak <58728039+ahmetkeremburak@users.noreply.github.com>
2023-07-01 13:57:24 +0300

    add | Interaction Handler

---
+++ InteractionHandler.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.Commands;
+using Discord.Interactions;
+using Discord.WebSocket;
+
+public class InteractionHandler{
+    private readonly DiscordSocketClient _client;
+    private readonly InteractionService _commands;
+    private readonly IServiceProvider _services;
+    // Construction injection is used to get DiscordSocketClient and Service container
+    public InteractionHandler(DiscordSocketClient client, InteractionService commands, IServiceProvider services){
+        _client = client;
+        _commands = commands;
+        _services = services;
+    }
+
+    // Will be used to add slash command modules
+    // Subscribes to "interaction created event"  
+    public async Task InitializeAsync(){
+        //add module
+        await _commands.AddModulesAsync(Assembly.GetEntryAssembly(),_services);
+        //Interaction created event
+        _client.InteractionCreated += HandleInteraction;
+    }
+
+    // InteractionCreated
+    private async Task HandleInteraction(SocketInteraction arg){
+        try
+        {
+            var ctx = new SocketInteractionContext(_client, arg);
+            await _commands.ExecuteCommandAsync(ctx, _services);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+}
